66fb4499fb8873b6009141f3a65688f403c138c0
rinklove <88640680+rinklove@users.noreply.github.com>
2024-01-18 09:03:33 +0900

    Silver5 날짜 계산

--- codingPractice/codingPractice/src/baekjoon/Silver5_날짜계산.java
+++ codingPractice/codingPractice/src/baekjoon/Silver5_날짜계산.java
@@ -9,24 +9,24 @@ public class Silver5_날짜계산 {
 		
 		// 1-16-16 / 2-17-17 / 3-18-18 / 4-19-19 / 5-20-1
 		//(N % 16) + (올림수) = 1 / (N % 29) = 16
-		int E = sc.nextInt();	//1 ~ 15
-		int S = sc.nextInt();	//1 ~ 28
-		int M = sc.nextInt();	//1 ~ 19
+		int E = sc.nextInt()-1;	//1 ~ 15 -> 0 ~ 14
+		int S = sc.nextInt()-1;	//1 ~ 28 -> 0 ~ 27
+		int M = sc.nextInt()-1;	//1 ~ 19 -> 0 ~ 18
 		sc.close();
 		
-		//올림수
-		int upE = 0;
-		int upS = 0;
-		int upM = 0;
-		
 		//현재 년도
-		int N = 1;
-		int currentE = 1;
-		int currentS = 1;
-		int currentM = 1;
+		int N = 0;
+		int currentE = 0;
+		int currentS = 0;
+		int currentM = 0;
 		
 		while(!(currentE == E && currentS == S && currentM == M)) {
-			
+			N++;
+			currentE = (N % 15);
+			currentS = (N % 28);
+			currentM = (N % 19);
 		}
+		
+		System.out.println(N+1);
 	}
 }
