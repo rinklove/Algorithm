6abd9a73573d756064b3cd151736a73f7b9f4cec
rinklove <88640680+rinklove@users.noreply.github.com>
2024-02-12 15:35:09 +0900

    2024. 2. 12.(문제 내용 추가)

--- codingPractice/codingPractice/src/baekjoon/silver3/숫자_정사각형.java
+++ codingPractice/codingPractice/src/baekjoon/silver3/숫자_정사각형.java
@@ -5,6 +5,14 @@ import java.util.StringTokenizer;
 
 public class 숫자_정사각형 {
 
+    /**
+     * N×M크기의 직사각형이 있다. 각 칸에는 한 자리 숫자가 적혀 있다.
+     * 이 직사각형에서 꼭짓점에 쓰여 있는 수가 모두 같은 가장 큰 정사각형을 찾는 프로그램을 작성하시오.
+     * 이때, 정사각형은 행 또는 열에 평행해야 한다.
+     * 
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
