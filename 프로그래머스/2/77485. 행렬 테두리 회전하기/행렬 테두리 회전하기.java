06f7f1ef453be037ae62232e655dece39efefcd7
junho <88640680+rinklove@users.noreply.github.com>
2024-05-27 19:54:01 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 5.75 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -0,0 +1,81 @@
+/**
+    2 2  
+    5 4
+    => 2열 2 -> 4 (y1, y2,| x1)
+    => 2 -> 5열 4 (x1, x2,| y2)
+    => 5열 4 -> 2 (y2, x2,| x2)
+    => 5 -> 2열 2 (x2, x1,| y1)
+
+*/
+class Solution {
+    private int[][] procession;
+    
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[] answer = new int[queries.length];        
+        
+        init(rows, columns);    //배열 초기화
+        int index = 0;
+        for(int[] query :queries) {
+            answer[index++] = rotateAndGetMin(query);
+        }
+        
+        return answer;
+    }
+    
+    private void init(int row, int column) {
+        procession = new int[row][column];
+        int number = 1;
+        for(int[] line : procession) {
+            for(int i = 0; i < line.length; i++) {
+                line[i] = number++;
+            }
+        }
+    }
+    
+    private int rotateAndGetMin(int[] query) {
+        //2 2 5 4 -> 1 1 4 3
+        minus1(query);
+        
+        int x1 = query[0];
+        int y1 = query[1];
+        int x2 = query[2];
+        int y2 = query[3];
+        
+        int tmp = procession[x1][y1];
+        int min = tmp;
+        
+        //왼쪽
+        for(int i = x1; i < x2; i++) {
+            procession[i][y1] = procession[i+1][y1];
+            min = Math.min(min, procession[i][y1]);
+        }
+        
+        //아래
+        for(int i = y1; i < y2; i++) {
+            procession[x2][i] = procession[x2][i+1];
+            min = Math.min(min, procession[x2][i]);
+        }
+        
+        //오른쪽
+        for(int i = x2; i > x1 ; i--) {
+            procession[i][y2] = procession[i-1][y2];
+            min = Math.min(min, procession[i][y2]);
+        }
+        
+        //위
+        for(int i = y2; i > y1 ; i--) {
+            procession[x1][i] = procession[x1][i-1];
+            min = Math.min(min, procession[x1][i]);
+        }
+        
+        procession[x1][y1+1] = tmp;
+        return min;
+    }
+    
+    
+    private void minus1(int[] query) {
+        for(int i = 0; i < query.length; i++) {
+            query[i]--;
+        }
+    }
+}
