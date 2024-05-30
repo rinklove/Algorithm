a98816daf064538439ed20711e2cde9f77754fdb
junho <88640680+rinklove@users.noreply.github.com>
2024-05-30 14:53:06 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.86 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -19,8 +19,13 @@ class Solution {
         //각 튜플의 S_i를 구하기
         int arr[] = getS_i(data, row_begin, row_end);
         
-        //구한 S_i의 xor 연산 수행한 후 값 반환하기      
-        return findXorValue(arr);
+        //구한 S_i의 xor 연산 수행하기  
+        for(int i : arr) {
+            answer = answer^i;
+        }
+        
+           
+        return answer;
     }
     
     private int[] getS_i(int[][] data, int row_begin, int row_end) {
@@ -38,57 +43,57 @@ class Solution {
         return s_arr;
     }
     
+//     ^연산자를 사용하지 않고 구현하기..
+//     private int findXorValue(int[] arr) {
+//         StringBuilder sb = new StringBuilder();
+//         //이진수 문자로 저장
+//         String[] binaryStrings = convertBinaryString(arr);
+        
+//         //각 자릿수에 대해서 xor 연산의 결과를 StrinBuilder에 저장
+//         for(int i = 0; i < binaryStrings[0].length(); i++) {
+//             char c = binaryStrings[0].charAt(i);
+//             for(int j = 1; j < binaryStrings.length; j++) {
+//                 c = c == binaryStrings[j].charAt(i) ? '0' : '1';
+//             }
+//             sb.append(c);
+//         }
+        
+//         //이진수 문자열을 정수로 반환
+//         return Integer.parseInt(sb.toString(), 2);
+//     }
     
-    private int findXorValue(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        //이진수 문자로 저장
-        String[] binaryStrings = convertBinaryString(arr);
-        
-        //각 자릿수에 대해서 xor 연산의 결과를 StrinBuilder에 저장
-        for(int i = 0; i < binaryStrings[0].length(); i++) {
-            char c = binaryStrings[0].charAt(i);
-            for(int j = 1; j < binaryStrings.length; j++) {
-                c = c == binaryStrings[j].charAt(i) ? '0' : '1';
-            }
-            sb.append(c);
-        }
-        
-        //이진수 문자열을 정수로 반환
-        return Integer.parseInt(sb.toString(), 2);
-    }
     
+//     private String[] convertBinaryString(int[] arr) {
+//         String[] binaryStrings = new String[arr.length];
+//         int max = 0; //변환한 이진수 문자열 중 가장 긴 길이
+        
+//         for(int i = 0; i < arr.length; i++) {
+//             binaryStrings[i] = Integer.toBinaryString(arr[i]);
+//             max = Math.max(max, binaryStrings[i].length());
+//         }
+        
+//         //이진수 문자열의 길이를 맞추기
+//         fitLength(binaryStrings, max);
+        
+//         return binaryStrings;
+//     }
     
-    private String[] convertBinaryString(int[] arr) {
-        String[] binaryStrings = new String[arr.length];
-        int max = 0; //변환한 이진수 문자열 중 가장 긴 길이
+//     /*
+//       0
+//       100
+//     */
+//     private void fitLength(String[] arr, int max) {
+//         StringBuilder sb = new StringBuilder();
+//         int index = 0;
         
-        for(int i = 0; i < arr.length; i++) {
-            binaryStrings[i] = Integer.toBinaryString(arr[i]);
-            max = Math.max(max, binaryStrings[i].length());
-        }
-        
-        //이진수 문자열의 길이를 맞추기
-        fitLength(binaryStrings, max);
-        
-        return binaryStrings;
-    }
-    
-    /*
-      0
-      100
-    */
-    private void fitLength(String[] arr, int max) {
-        StringBuilder sb = new StringBuilder();
-        int index = 0;
-        
-        //가장 긴 문자열 길이에 맞춰서 앞자리에 0 추가하기
-        for(String s : arr) {
-            for(int i = 0; i < max - s.length(); i++) {
-                sb.append("0");
-            }
-            sb.append(s);
-            arr[index++] = sb.toString();
-            sb.setLength(0);
-        }
-    }
+//         //가장 긴 문자열 길이에 맞춰서 앞자리에 0 추가하기
+//         for(String s : arr) {
+//             for(int i = 0; i < max - s.length(); i++) {
+//                 sb.append("0");
+//             }
+//             sb.append(s);
+//             arr[index++] = sb.toString();
+//             sb.setLength(0);
+//         }
+//     }
 }
