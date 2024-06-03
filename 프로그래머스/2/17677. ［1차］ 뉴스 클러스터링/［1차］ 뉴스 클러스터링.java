82b37fd84c38bd571906f1d890a7cd93c5c4ce6b
junho <88640680+rinklove@users.noreply.github.com>
2024-06-03 17:42:16 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.34 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -9,21 +9,20 @@ class Solution {
     private final List<String> listA = new ArrayList<>();
     private final List<String> listB = new ArrayList<>();
     public int solution(String str1, String str2) {
-        int answer = 0;
-        
+        //원문 가공
         str1 = convert(str1);
         str2 = convert(str2);
         
+        //A, B의 다중원소 설정
         setElement(str1, str2);
         
-        answer = findJacard();
-        
-        return answer;
+        //자카드 유사도 * 65536 값 구하기
+        return findJacard();
     }
     
     private int findJacard() {
-        float union = 0;
-        float intersect = 0;
+        float union = 0;        //합집합
+        float intersect = 0;    //교집합
         
         int sizeA = listA.size();
         for(int i = 0; i < sizeA; i++) {
@@ -32,13 +31,14 @@ class Solution {
             if(listB.remove(find)) {
                 intersect++;
             }
-            System.out.print("find = " + find);
-            System.out.print(", union = " + union);
-            System.out.println(", intersect = " + intersect);
+            // System.out.print("find = " + find);
+            // System.out.print(", union = " + union);
+            // System.out.println(", intersect = " + intersect);
         }
         
         union += listA.size() + listB.size();
         
+        //공집합끼리 비교하는 경우 항상 일치하므로 자카드 유사도는 1이 됨.
         return union == 0 ? 65536 : (int) (65536 * intersect / union);
     }
     
@@ -52,6 +52,7 @@ class Solution {
         
         for(int i = 0; i < str2.length()-1; i++) {
             String substr = str2.substring(i, i+2);
+            //문자로만 이루어진 다중원소를 추가
             if(!substr.contains(" ")) {
                 listB.add(substr);            
             }
@@ -59,6 +60,7 @@ class Solution {
     }
     
     private String convert(String str) {
+        //특수문자를 공백으로 치환한다.
         return str.toLowerCase().replaceAll("[^a-z]", " ");
     }
 }
