bdfa333372cba3f7b6ee8ff73178ee57f65f0e44
junho <88640680+rinklove@users.noreply.github.com>
2024-06-19 16:16:38 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.35 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            answer += check(s);
+            s = s.substring(1) + s.substring(0, 1);
+        }
+        
+        return answer;
+    }
+    
+    private int check(String str) {
+        Stack<Character> stack = new Stack<>();
+        int index = 0;
+        try {
+            for(int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                switch(c) {
+                    case ']':
+                        if(stack.peek() != '[') return 0;
+                        stack.pop(); break;
+                    case '}':
+                        if(stack.peek() != '{') return 0;
+                        stack.pop(); break;
+                    case ')': 
+                        if(stack.peek() != '(') return 0;
+                        stack.pop(); break;
+                    default: stack.push(c); break;
+                }
+            }
+        } catch(Exception e) {
+            return 0;
+        }
+        
+        return stack.isEmpty() ? 1: 0;
+    }
+}
