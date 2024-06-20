ddd009bcb6140a3fec52728039159d125833fdc2
junho <88640680+rinklove@users.noreply.github.com>
2024-06-20 18:03:57 +0900

    [level 2] Title: n^2 배열 자르기, Time: 4.82 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,15 +1,3 @@
-/*
-    1 2 3 4
-    2 2 3 4
-    3 3 3 4
-    4 4 4 4
-    ----
-    1. 값을 채워 넣나? 
-    arr[0] = 1
-    arr[4] = 2
-    arr[8] = 3
-    arr[15] = 4
-*/
 class Solution {
     public int[] solution(int n, long left, long right) {
         int[] answer = new int[(int) (right - left) + 1];
