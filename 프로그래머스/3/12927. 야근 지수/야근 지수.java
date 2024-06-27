15718e5a32a901e8687a613f1dec5c8a9680dfda
junho <88640680+rinklove@users.noreply.github.com>
2024-06-27 14:30:15 +0900

    [level 3] Title: 야근 지수, Time: 101.23 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    
+    public long solution(int n, int[] works) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((w1, w2) -> w2 - w1);
+        setWorks(pq, works);
+        work(pq, n);
+        return getFatigue(pq);
+    }
+    
+    private long getFatigue(PriorityQueue<Integer> pq) {
+        long sum = 0;
+         for(int w : pq) {
+            sum += (long) Math.pow(w, 2);
+        }
+        return sum;
+    } 
+    
+    private void work(PriorityQueue<Integer> pq, int n) {
+        while(!pq.isEmpty() && n != 0) {
+            int poll = pq.poll();
+            poll--;
+            if(poll != 0) {
+                pq.offer(poll);            
+            }
+            n--;
+        }
+    }
+    
+    private void setWorks(PriorityQueue<Integer> pq, int[] works) {
+        for(int w : works) {
+            pq.offer(w);
+        }
+    }
+}
