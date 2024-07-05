3dbb7b3aef09d7b346ce323abef9b92ec22679db
junho <88640680+rinklove@users.noreply.github.com>
2024-07-05 14:23:28 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 60.78 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -11,17 +11,18 @@ class Solution {
         long sum2 = getSum(queue2, q2);
         if((sum1+sum2) % 2 == 1) return answer;
         
-        for(int count = 0; count < cycle; count++) {
-            if(!(sum1 != 0 && sum2 != 0)) return -1;
-            
-            if(sum1 == sum2) return count;
+        for(int count = 0; count < cycle; count++) {            
+            int element;
+            if(sum1 == sum2) {
+                return count;
+            }
             else if(sum1 > sum2) {
-                int element = q1.poll();
+                element = q1.poll();
                 q2.offer(element);
                 sum1 -= element;
                 sum2 += element;
             } else {
-                int element = q2.poll();
+                element = q2.poll();
                 q1.offer(element);
                 sum1 += element;
                 sum2 -= element;
