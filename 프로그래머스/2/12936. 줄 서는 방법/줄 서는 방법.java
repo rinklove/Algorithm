837d427d7f64e9002f3e7854396081180a75a51b
junho <88640680+rinklove@users.noreply.github.com>
2024-07-09 16:35:34 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.05 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -1,18 +1,16 @@
-/*
-    1 = 1
-    2 = 2
-    3 = 6
-*/
 import java.util.List;
 import java.util.ArrayList;
 class Solution {
     public int[] solution(int n, long k) {
         int[] answer = new int[n];
+        //1부터 n까지 들어있는 리스트 및 n! 값 생성 
         List<Integer> list = getList(n);
         long f = factorial(n);
         
+        //원활한 인덱스 접근을 위해서 k값 감소
         k--;
         for(int i = 0; i < answer.length; i++) {
+            //n-1명의 사람을 줄세우는 경우의 수
             long tmp = f / n;
             answer[i] =  list.remove((int) (k / tmp));
             k %= tmp;
