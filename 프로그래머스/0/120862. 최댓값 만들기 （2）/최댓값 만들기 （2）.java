90d214e9ac72347b847dbf53ac54e9f63bbd4fcc
junho <88640680+rinklove@users.noreply.github.com>
2024-07-16 16:18:04 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.55 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
         
-        numbers = Arrays.stream(numbers).sorted().toArray();
+        Arrays.sort(numbers);
 		
 		int max1 = numbers[0] * numbers[1];
 		int max2 = numbers[numbers.length-1] * numbers[numbers.length-2];
