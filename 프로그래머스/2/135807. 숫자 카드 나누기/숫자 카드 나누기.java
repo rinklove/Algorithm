c0534a957ef82af74f7a51a82d946a4b2c07d981
junho <88640680+rinklove@users.noreply.github.com>
2024-07-20 17:46:52 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 930.86 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,5 +1,13 @@
 import java.util.Arrays;
-
+/*
+    방안 1. 배열의 중복을 제거+내림차순 정렬하고 
+            제일 큰 수부터 시작해서 값을 감소시키며 최대 공약수를 찾음 -> 성공
+            하지만 어차피 제일 큰수를 제외한 나머지 수는 안나누어지는 점을 발견 
+            + 처리 시간도 긴 문제 발생
+            
+    방안 2. 배열의 오름차순으로 정렬 후, 제일 작은 수부터 시작해서 값을 감소 -> 성공
+            방안 1보다 처리 속도도 빨라졌음.
+*/
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
         int answer = 0;
@@ -8,9 +16,9 @@ class Solution {
     }
     
     private int compare(int[] arrayA, int[] arrayB) {
-        //두 배열의 중복된 원소를 제거하고 내림차순으로 정렬.
-        manufacture(arrayA);
-        manufacture(arrayB);
+        //두 배열을 오름차순으로 정렬.
+        sort(arrayA);
+        sort(arrayB);
         
         //왼쪽 배열에서 모두 나누어지면서 오른쪽 배열에서 모두 나누어지지 않는 최댓값을 구함 
         int maxA = findMax(arrayA, arrayB);
@@ -18,7 +26,8 @@ class Solution {
         return Math.max(maxA, maxB);
     }
     
-    private void manufacture(int[] array) {
+    private void sort(int[] array) {
+        // Arrays.sort(Arrays.stream(array).distinct().toArray());
         Arrays.sort(array);
     }
     
