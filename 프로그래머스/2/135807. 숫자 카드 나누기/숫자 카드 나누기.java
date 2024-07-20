cbdda2b4bbc4eb448c82f50de25909f7e39b093a
junho <88640680+rinklove@users.noreply.github.com>
2024-07-20 17:32:34 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 1067.36 ms, Memory: 227 MB -BaekjoonHub

---
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] arrayA, int[] arrayB) {
+        int answer = 0;
+        answer = compare(arrayA, arrayB);
+        return answer;
+    }
+    
+    private int compare(int[] arrayA, int[] arrayB) {
+        //두 배열의 중복된 원소를 제거하고 내림차순으로 정렬.
+        manufacture(arrayA);
+        manufacture(arrayB);
+        
+        int maxA = findMax(arrayA, arrayB);
+        int maxB = findMax(arrayB, arrayA);
+        System.out.println("maxA = " + maxA + ", maxB = " + maxB);
+        return Math.max(maxA, maxB);
+    }
+    
+    private void manufacture(int[] array) {
+        array = Arrays.stream(array)
+            .distinct()
+            .boxed()
+            .sorted((i1, i2) -> i2 - i1)
+            .mapToInt(i->i)
+            .toArray();
+    }
+    
+    private int findMax(int[] target, int[] opposite) {
+        int start = target[0];
+        while(start > 1) {
+            if(isGcd(target, start) && mod(opposite, start)) {
+                return start;
+            }
+            start--;
+        }
+        return 0;
+    }
+    
+    private boolean isGcd(int[] arr, int num) {
+        for(int n : arr) {
+            if(n % num != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean mod(int[] arr, int num) {
+        for(int n : arr) {
+            if(n % num == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
