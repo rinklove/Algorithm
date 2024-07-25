c7edc4845fd515914ac07da523a4aae218e4ba17
junho <88640680+rinklove@users.noreply.github.com>
2024-07-25 12:14:47 +0900

    [level 1] Title: 옹알이 (2), Time: 11.00 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,12 +1,9 @@
+import java.util.Arrays;
 class Solution {
     public int solution(String[] babbling) {
-        int answer = 0;
-        
-        for(int i =0; i < babbling.length; i++) {			
-			babbling[i] = babbling[i].replaceAll("ayaaya|yeye|woowoo|mama", " ");
-			babbling[i] = babbling[i].replaceAll("aya|ye|woo|ma", "");
-			if(babbling[i].equals("")) answer++;
-		}
-        return answer;
+        return (int) Arrays.stream(babbling)
+            .map(b -> b.replaceAll("ayaaya|yeye|woowoo|mama", " "))
+            .map(b -> b.replaceAll("aya|ye|woo|ma", ""))
+            .filter(b -> b.isEmpty()).count();
     }
 }
