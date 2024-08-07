7457b7ec58182375557d1e6e203e41bfa2845b07
junho <88640680+rinklove@users.noreply.github.com>
2024-08-07 23:28:55 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 30.79 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -0,0 +1,25 @@
+/*
+    x^2 + y^2 = 4
+    
+    x^2 + y^2 = 9
+    x^2 = 9 - y^2
+    x = 루트(9 - y^2)
+    
+    4 <= x1^2 + y1^2 <= 9
+*/
+class Solution {
+    public long solution(int r1, int r2) {
+        long answer = 0;
+        long area1 = (long)r1*r1; 
+        long area2 = (long)r2*r2;
+        long area = 0;
+        for(int i = 0; i < r2; i++) {
+            long outerX = (long) Math.floor(Math.sqrt(area2 - Math.pow (i,2)));
+            long innerX = (long) Math.ceil(Math.max(0, Math.sqrt(area1 - Math.pow (i,2))));
+
+            answer += outerX - innerX + 1;
+        }
+        answer++;
+        return (answer - (r2-r1+1))*4;
+    }
+}
